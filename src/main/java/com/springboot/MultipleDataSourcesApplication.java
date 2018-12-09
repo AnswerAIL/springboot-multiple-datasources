1627a034d2360db3f34dcc9da12e7557a3fe5e8a
L.Answer <answer_ljm@163.com>
2018-12-09 16:57:28 +0800

    配置多个监听端口

--- src/main/java/com/springboot/MultipleDataSourcesApplication.java
+++ src/main/java/com/springboot/MultipleDataSourcesApplication.java
@@ -1,9 +1,12 @@
 package com.springboot;
 
+import com.springboot.datasources.config.EmbeddedTomcatConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(EmbeddedTomcatConfiguration.class)
 public class MultipleDataSourcesApplication {
 
 	public static void main(String[] args) {
